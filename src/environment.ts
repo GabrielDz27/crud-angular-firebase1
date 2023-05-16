ac673ac091c6d1cceddf679e9d174f06fdc2b2f1
KarlMarxLendario <110194151+GabrielDz27@users.noreply.github.com>
2023-05-16 15:24:18 -0300

    Add files via upload

---
+++ src/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBhYvwalTjbDRaMNEUAsAGmjJOyG-ffvp8",
+    authDomain: "crud-angular-firebase-f6e10.firebaseapp.com",
+    databaseURL: "https://crud-angular-firebase-f6e10-default-rtdb.firebaseio.com",
+    projectId: "crud-angular-firebase-f6e10",
+    storageBucket: "crud-angular-firebase-f6e10.appspot.com",
+    messagingSenderId: "252287140426",
+    appId: "1:252287140426:web:77f73d6f228ede18949cb3"
+}
+};
